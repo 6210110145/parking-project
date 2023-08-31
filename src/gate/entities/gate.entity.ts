b491fc31c3a7fdf0175015bd358dfc17fb22bf19
6210110145 <6210110145@psu.ac.th>
2023-08-31 15:46:27 +0700

    headage

--- src/gate/entities/gate.entity.ts
+++ src/gate/entities/gate.entity.ts
@@ -1,4 +1,5 @@
-import { BaseEntity, PrimaryGeneratedColumn } from "typeorm";
+import { ColdObservable } from "rxjs/internal/testing/ColdObservable";
+import { BaseEntity, Column, PrimaryGeneratedColumn } from "typeorm";
 
 export class Gate extends BaseEntity{
     @PrimaryGeneratedColumn({
@@ -6,4 +7,31 @@ export class Gate extends BaseEntity{
         name: 'gate_id'
     })
     gate_id: number
+
+    @Column({
+        name: 'name',
+        nullable: false
+    })
+    gate_name: string
+
+    @Column({
+        name: 'in-out gates',
+        nullable: false
+    })
+    gate_type: string
+
+    @Column({
+        name: 'time'
+    })
+    gate_time: Date
+
+    @Column({
+        name: 'เลขป้ายทะเบียน'
+    })
+    car_license: string
+
+    @Column({
+        name: 'จังหวัด'
+    })
+    car_province: string
 }
